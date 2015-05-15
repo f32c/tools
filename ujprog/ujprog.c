1f8618b895993f06c97857167de7c89d10641c37
gornjas <zec@fer.hr>
2015-05-15 11:49:36 +0200

    BSD license for ujprog.

--- ujprog/ujprog.c
+++ ujprog/ujprog.c
@@ -1,18 +1,34 @@
 /*
  * FT-232R USB JTAG programmer
  *
- * (c) 2010 - 2015 Marko Zec <zec@fer.hr>
+ * Copyright (c) 2010 - 2015 Marko Zec, University of Zagreb
  *
- * This software is NOT freely redistributable, neither in source nor in
- * binary format.  Usage in binary format permitted exclusively for
- * programming FER's FPGA boards.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
  * TODO:
  * 
  * - WIN32: check for USB device string description
- *
- * - Cryptographically bind USB serial number with FTDI chip ID
  *
  * - JTAG scan / identify chain on entry.
  *
